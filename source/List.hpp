2e87569ca92c4d0f98cc6a83e2a4aa29b957afe7
valentinanaghavi <gissu.valentina.naghavi@uni-weimar.de>
2018-06-11 11:12:34 +0200

    add 4.5

--- source/List.hpp
+++ source/List.hpp
@@ -20,9 +20,73 @@ template <typename T>
 class ListIterator
 {
     public :
-    // not implemented yet
-    // do not forget about the initialiser lists !
+    using Self = ListIterator<T>;
+    using value_type = T;
+    using pointer = T*;
+    using reference = T&;
+    using difference_type = ptrdiff_t;
+    using iterator_category = std::bidirectional_iterator_tag;
+    //Aufgabe (4.5)
+    ListIterator():
+    node{nullptr}
+    {};
+
+    ListIterator(ListNode<T>*n):
+    node{n}
+    {};
+
+    reference operator*() const //Wert der Referenz zurueckgeben
+    {
+        return (*node).value;
+    } 
+
+    pointer operator->() const // Pointer/Wert - Adresse zurueckgeben
+    {
+        return &(node -> value);
+    } 
+    Self& operator++() // iterator++
+    {
+        node = (*node).next;
+        return *this;
+    }
+    Self operator++( int ) //erst ausgabe des Wertes , folgend i++
+    {
+        Self wert = *this;
+        ++(*this);
+        return wert;
+    } 
+    bool operator==( Self const &x ) const //Vergleich 
+    {
+        if (node == x.node)  
+		{							  
+			return true;            
+		}
+		else
+        {
+            return false;
+        }
+    } 
+    bool operator!=( Self const &x ) const //Ungleich
+   {
+        if (node != x.node) 
+		{							  
+			return true;            
+		}
+		else
+        {
+            return false;
+        }
+    } 
+    Self next() const
+    {
+        if(node)
+            return ListIterator(node->next);
+        else
+            return ListIterator(nullptr);
+    }
+
     private :
+    // The Node the iterator is pointing to
     ListNode <T>* node ;
 };
 
@@ -55,10 +119,18 @@ class List
     last_{nullptr}
     {}
 
-    ~List()                 // (4.5) Destructor 
+    ~List()                 // (4.4) Destructor 
     {
         clear();
     }
+
+    List(std::initializer_list<T> list) //initialisierungsliste
+        {
+            for(auto i : list)
+            {
+                push_back(i);
+            }
+};
 
     bool empty () const     // (4.2)
     {
@@ -88,7 +160,7 @@ class List
         }
         else
         {
-            return first_->value;
+            return (*first_).value;
         }
     }
 
@@ -101,35 +173,86 @@ class List
         }
         else
         {
-            return last_->value;    //Wert worauf die Referenz zeigt   
+            return (*last_).value;    //Wert worauf die Referenz zeigt   
         }
     }
 
-    void push_front(T const& value)
+    void push_front(T const& v)
     {
-        if (empty != 0 )
+        ListNode<T>* node = new ListNode<T>;
+        (*node).value = v;       //node -> value
+
+        if (empty() == true )
+        {
+            last_ = node;
+        }
+        else
+        {
+            (*first_).prev = node;
+            (*node).next = first_;
+            first_ = node;
+        }
+        first_ = node;
+        size_++;
+        node = nullptr;
+
     }
 
-    void push_back(T const& value)
-    {
 
+
+    void push_back(T const& v)
+    {
+        ListNode<T>* node = new ListNode<T>;
+        (*node).value = v;       //node -> value
+
+        if (empty() == true )
+        {
+            first_ = node;
+            last_ = first_;
+        }
+        else
+        {
+            (*last_).next = node;
+            (*node).prev = last_;
+            last_ = node;
+        }
+        size_++;
+        node = nullptr;
     }
 
     void pop_front()
     {
-        if (empty != 0 )
+        if (empty() == true)
+        {
+            std::cout<<"empty list";            
+        }
+        else
+        {
+            first_ = (*first_).next;
+            (*first_).prev = nullptr;
+            size_--;
+        }
     }
 
     void pop_back()
     {
-
+       if (empty() == true)
+        {
+            std::cout<<"empty list";            
+        }
+        else
+        {
+            last_ = (*last_).prev;
+            (*last_).next = nullptr;
+            size_--;
+        }
     }
 
-    void clear()
+    void clear()        //aufgabe 4.4
     {
-        if (empty == false)
+        if (empty() == false)
         {
-            while (empty == false)
+            while (empty() == false)
             {
                 pop_back();
             }
@@ -139,6 +262,15 @@ class List
             std::cout<<"empty list";  
         }
     }
+    //aufgabe(4.6)
+    ListIterator<T> begin() const
+    {		
+			return ListIterator<T>(first_);
+	}
+	ListIterator<T> end() const 
+    {		
+			return ListIterator<T>(nullptr);
+    }
 
 
     private :
@@ -146,5 +278,39 @@ class List
     ListNode <T>* first_;
     ListNode <T>* last_ ;
 };
+//aufgabe (4.7)
+template<typename T>
+bool operator ==(List<T>const& xs,List<T>const& ys)
+{
+    if(xs.size()!=ys.size())
+    {
+        return false;
+    }
+    else
+    {
+    ListIterator<T> x = xs.begin();
+    ListIterator<T> y = ys.begin();
 
+    for(int i = 0 ; i <xs.size() ; i++)    
+    {
+        if(*x != *y )
+        {
+            return false;
+        }
+        else
+        {
+        x++;
+        y++;
+        }
+    }
+        return true;
+    }
+
+}
+
+template<typename T>
+bool operator !=(List<T>const& xs,List<T>const& ys)
+{
+    return !(xs == ys);
+}
 # endif // #define BUW_LIST_HPP
--- source/ListTest.cpp
+++ source/ListTest.cpp
@@ -5,10 +5,6 @@
 #include <iostream>
 
 
-int main(int argc, char* argv[])
-{
-    return Catch::Session().run(argc,argv);
-}
 
 TEST_CASE("size and empty","[List]")
 {
@@ -17,29 +13,32 @@ TEST_CASE("size and empty","[List]")
     REQUIRE(list1.size()==0);
 }
 
+
 TEST_CASE("add an element with push_front","[modifiers]")
 {
         List<int> list2;
+        list2.push_front(2);
         list2.push_front(42);
         REQUIRE(42==list2.front());
-
 }
 
 TEST_CASE("add an element with push_back","[modifiers]")
 {
-        List<int> list2{42};
-
-        list2.push_back(2);
-        REQUIRE(2==list2.back());
+        List<int> list22;
+        list22.push_back(42);
+        list22.push_back(2);
+        REQUIRE(2==list22.back());
 
 }
 
 TEST_CASE("remove an element with pop_front","[modifiers]")
 {
-        List<int> list3{3,4};
+        List<int> list3;
+        list3.push_back(3);
+        list3.push_back(4);
 
         list3.pop_front();
-        REQUIRE(4==list2.front());
+        REQUIRE(4==list3.front());
 
         list3.pop_front();
         REQUIRE(list3.empty());
@@ -48,7 +47,10 @@ TEST_CASE("remove an element with pop_front","[modifiers]")
 
 TEST_CASE("remove an element with pop_back","[modifiers]")
 {
-        List<int> list4{4,3,7};
+        List<int>list4;
+        list4.push_back(4);
+        list4.push_back(3);
+        list4.push_back(7);
 
         list4.pop_back();
         REQUIRE(3==list4.back());
@@ -68,13 +70,36 @@ TEST_CASE("should be empty after clearing","[modifiers]")
         REQUIRE(list.empty());
 }
 
-TEST_CASE("return begin and end", "[modifiers]")
+TEST_CASE("should be an empty range after default construction", "[iterators]")
 {
-        List<int> list5{2,4,99,12};
+    List<int> list;
+    auto b = list.begin();
+    auto e = list.end();
 
-
-        REQUIRE(2==list5.begin());
-        REQUIRE(12==list5.end());
-
+    REQUIRE(b == e);
 }
 
+TEST_CASE("provide acces to the first element with begin", "[iterators]")
+{
+    List<int> list;
+    list.push_front(42);
+
+    REQUIRE(42 == *list.begin());
+}
+
+TEST_CASE ( " copy constructor " , " [ constructor ] " )
+{
+    List<int>list;
+    list.push_front(1);
+    list.push_front(2);
+    list.push_front(3);
+    list.push_front(4);
+    List<int>list2{list};
+    REQUIRE(list==list2);
+}
+
+
+int main(int argc, char* argv[])
+{
+    return Catch::Session().run(argc,argv);
+}
